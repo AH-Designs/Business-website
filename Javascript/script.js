31e5209c414f7eebb8676410fa43c72f20e10b43
ah-designs <ahdesigns@outlook.com>
2016-04-09 12:22:15 +0100

    social media icons made responsive, centered in the footer when the site is viewed on a smaller device, jquery for my services page written.

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,80 @@
-$( ".read-more" ).click(function(){
 
-$(".div-appear-box").toggle();
+// carpentry div
+
+$( ".read-more-carpentry" ).click(function(){
+	$(this).parent().next(".div-appear-box");
+	$(".carpentry-div").slideToggle("slow"); 
 
 });
 
+
+
+$( ".carpentry-div" ).click(function(){
+
+	$(".carpentry-div").slideToggle("slow"); 
+
+});
+
+//plumbing div
+
+$( ".read-more-plumbing" ).click(function(){
+
+	$(".plumbing-div").slideToggle("slow"); 
+
+});
+
+
+
+$( ".plumbing-div" ).click(function(){
+
+	$(".plumbing-div").slideToggle("slow"); 
+
+});
+
+//electrical div
+
+$( ".read-more-electrical" ).click(function(){
+
+	$(".electrical-div").slideToggle("slow"); 
+
+});
+
+
+
+$( ".electrical-div" ).click(function(){
+
+	$(".electrical-div").slideToggle("slow"); 
+
+});
+
+//painting div
+
+$( ".read-more-painting" ).click(function(){
+
+	$(".painting-div").slideToggle("slow"); 
+
+});
+
+
+
+$( ".painting-div" ).click(function(){
+
+	$(".painting-div").slideToggle("slow"); 
+
+});
+
+//maintenance div
+
+$( ".read-more-maintenance" ).click(function(){
+
+	$(".maintenance-div").slideToggle("slow"); 
+
+});
+
+
+
+$( ".maintenance-div" ).click(function(){
+
+	$(".maintenance-div").slideToggle("slow"); 
+
+});
